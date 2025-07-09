7d354138effc2ccb9a628eedd03bac744fb8a1b9
itaiglikman <itaig1998@gmail.com>
2025-07-09 19:38:59 +0300

    feat: easy/calculator solution

--- Easy/calculator.js
+++ Easy/calculator.js
@@ -11,8 +11,20 @@ Notes
 If the input tries to divide by 0, return: "Can't divide by 0!"
 */
 
-function calculator( /*args*/ ) {
-  //your code
+function calculator(x, operator, y) {
+    switch (operator) {
+        case "+":
+            return x + y;
+        case "-":
+            return x - y;
+        case "*":
+            return x * y;
+        case "/":
+            if (y === 0) return "Can't divide by 0!";
+            return x / y;
+        default:
+            return "Invalid operator";
+    }
 }
 
 exports.solution = calculator;
