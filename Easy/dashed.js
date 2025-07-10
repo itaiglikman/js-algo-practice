dd4ff743ee3deefb508ea1ac99c494b4d2699dd9
itaiglikman <itaig1998@gmail.com>
2025-07-10 19:51:30 +0300

    feat: implement dashed function to add dashes around vowels in a string

--- Easy/dashed.js
+++ Easy/dashed.js
@@ -10,8 +10,37 @@ Notes
 A string can contain uppercase and lowercase vowels.
 */
 
-function dashed( /*args*/ ) {
-  //your code
+function dashed(str) {
+    const vowelsSet = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    let newStr = "";
+    let lastVowelIndex = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (vowelsSet.has(str[i])) {
+            let strBefore = str.substring(lastVowelIndex, i);
+            newStr = newStr.concat(strBefore,`-${str[i]}-`);
+            lastVowelIndex = i+1;
+        }
+    }
+    let leftOver = str.substring(lastVowelIndex);
+    newStr = newStr.concat(leftOver);
+
+    return newStr;
 }
+
+// option 2
+/**
+ * function dashed(str) {
+    const vowels = new Set('aeiouAEIOU');
+    let result = '';
+    for (let char of str) {
+        if (vowels.has(char)) {
+            result += '-' + char + '-';
+        } else {
+            result += char;
+        }
+    }
+    return result;
+}
+ */
 
 exports.solution = dashed;
