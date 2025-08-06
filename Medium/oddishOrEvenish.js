041d22c12db6d9320dee5f9c7c6b02a9422c693a
itaiglikman <itaig1998@gmail.com>
2025-08-06 17:04:06 +0300

    oddishOrEvenish

--- Medium/oddishOrEvenish.js
+++ Medium/oddishOrEvenish.js
@@ -1,5 +1,8 @@
 /*
-Create a function that determines whether a number is Oddish or Evenish. A number is Oddish if the sum of all of its digits is odd, and a number is Evenish if the sum of all of its digits is even. If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
+Create a function that determines whether a number is Oddish or Evenish. 
+A number is Oddish if the sum of all of its digits is odd, 
+and a number is Evenish if the sum of all of its digits is even. 
+If a number is Oddish, return "Oddish". Otherwise, return "Evenish".
 
 For example, oddishOrEvenish(121) should return "Evenish", since 1 + 2 + 1 = 4. oddishOrEvenish(41) should return "Oddish", since 4 + 1 = 5.
 
@@ -11,8 +14,11 @@ oddishOrEvenish(373) ➞ "Oddish"
 oddishOrEvenish(4433) ➞ "Evenish"
 */
 
-function oddishOrEvenish( /*args*/ ) {
-  //your code
+function oddishOrEvenish(num) {
+    const sumOfDig = num.toString().split('').reduce((sum, char) => sum += Number(char), 0)
+    return sumOfDig % 2 === 0 ? "Evenish" : "Oddish";
 }
+
+oddishOrEvenish(43)
 
 exports.solution = oddishOrEvenish;
