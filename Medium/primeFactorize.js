cebbb6174f3cef14c3bcd1ab677970ac65ee777b
itaiglikman <itaig1998@gmail.com>
2025-08-10 18:06:33 +0300

    primeFactorize naive solution

--- Medium/primeFactorize.js
+++ Medium/primeFactorize.js
@@ -1,5 +1,7 @@
 /*
-Write a program to find all the prime factors of a given number. The program must return an array containing all the prime factors, sorted in ascending order. Remember that 1 is neither prime nor composite and should not be included in your output array.
+Write a program to find all the prime factors of a given number. 
+The program must return an array containing all the prime factors, sorted in ascending order.
+Remember that 1 is neither prime nor composite and should not be included in your output array.
 
 Examples
 primeFactorize(25) ➞ [5, 5]
@@ -12,8 +14,28 @@ Output array must be sorted in ascending order
 The only positive integer which is neither prime nor composite is 1. Return an empty array if 1 is the input.
 */
 
-function primeFactorize( /*args*/ ) {
-  //your code
+
+// naive solution
+function primeFactorize(num) {
+    if (num === 1) return [];
+    let factorArr = []
+    for (let i = 2; i <= num / 2; i++) {
+        if (num % i === 0) factorArr.push(i);
+    }
+
+    factorArr = factorArr.sort((a, b) => a - b).filter(n => isPrime(n));
+
+    function isPrime(n) {
+        if (n < 2) return false;
+        for (let i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i === 0) return false;
+        }
+        return true;
+    }
+
+    console.log(factorArr);
 }
+
+primeFactorize(1000000000)
 
 exports.solution = primeFactorize;
