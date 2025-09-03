6f0f85c1ce6ef7f1757a2fd3650335e91562f7eb
itaiglikman <itaig1998@gmail.com>
2025-09-03 14:54:21 +0300

    very hard - matrix

--- Very_Hard/isWristband.js
+++ Very_Hard/isWristband.js
@@ -41,8 +41,97 @@ isWristband([
 // Part of diagonal right wristband.
 */
 
-function isWristband( /*args*/ ) {
-  //your code
+function isWristband(mat) {
+    /**
+    check each option:
+    1. row - arr[0][0] = arr[0][1] 
+    2. col - arr[0][0] = arr[1][0]
+    3. diagonal left - arr[0][0] = arr[1][1]
+    3. diagonal right - arr[1][0] = arr[0][1]
+     */
+    console.log(mat.length);
+    let result = false;
+    if (mat[0][0] === mat[0][1]) {
+        result = checkRow(mat);
+        console.log('row', result);
+    }
+    if (mat[0][0] === mat[1][0]) {
+        console.log(mat[0][0], mat[1][0]);
+        result = checkCol(mat);
+        console.log('col', result);
+    }
+    if (mat[0][0] === mat[1][1]) {
+        result = checkDiagonalLeft(mat)
+        console.log('diagonal left', result);
+    };
+    //     if (mat[1][0] === mat[0][1]) checkDiagonalRight(mat);
+    console.log(result);
 }
+
+function checkRow(mat) {
+    for (let i = 0; i < mat.length; i++) {
+        const first = mat[i][0]
+        for (let j = 0; j < mat[i].length; j++) {
+            if (mat[i][j] !== first) return false;
+        }
+    }
+    return true;
+}
+
+function checkCol(mat) {
+    for (let i = 0; i < mat[0].length; i++) {
+        const first = mat[0][i]
+        for (let j = 0; j < mat.length; j++) {
+            console.log(mat[j][i]);
+            if (mat[j][i] !== first) return false;
+        }
+    }
+    return true;
+}
+
+function checkDiagonalLeft(mat) {
+    for (let i = 0; i < mat.length; i++) {
+        const first = mat[i][i];
+        for (let j = i + 1; j < mat[0].length; j++) {
+            console.log(first);
+            console.log(mat[j][j]);
+            if (mat[j][j] !== first) return false;
+        }
+    }
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+    // for (let i = 1; i < mat.length; i++) {
+    //     const first = mat[i][i];
+    //     for (let j = i + 1; j < mat[0].length; j++) {
+    //         console.log(first);
+    //         console.log(mat[j][j]);
+    //         if (mat[j][j] !== first) return false;
+    //     }
+    // }
+    return true;
+}
+
+// isWristband([
+//     ["A", "A"],
+//     ["B", "B"],
+//     ["C", "C"]
+// ])
+
+// isWristband([
+//     ["A", "B"],
+//     ["A", "B"],
+//     ["A", "B"]
+// ])
+
+isWristband([
+ ["A", "B", "C"],
+ ["C", "A", "B"],
+ ["B", "C", "A"],
+ ["A", "B", "C"]
+])
+
+
 
 exports.solution = isWristband;
--- Very_Hard/matrix.js
+++ Very_Hard/matrix.js
@@ -21,8 +21,40 @@ matrix(4) ➞ [
 ]
 */
 
-function matrix( /*args*/ ) {
-  //your code
+const directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]; // right, down, left, up
+
+function matrix(n) {
+    const mat = Array(n).fill().map(() => Array(n).fill(0));
+    let currDirection = 0, counter = 1, row = 0, col = 0;
+
+    while (counter <= n * n) {
+        mat[row][col] = counter++;
+        let nextRow = row + directions[currDirection][0];
+        let nextCol = col + directions[currDirection][1];
+        if (nextRow < 0 || nextRow >= n || nextCol < 0 || nextCol >= n || mat[nextRow][nextCol] !== 0) {
+            currDirection = changeDirection(currDirection);
+        }
+        [row, col] = getCord(row, col, currDirection);
+    }
+    console.table(mat);
+    return mat;
 }
+
+function changeDirection(currDirection) {
+    switch (currDirection) {
+        case 0: return 1;
+        case 1: return 2;
+        case 2: return 3;
+        case 3: return 0;
+    }
+}
+
+function getCord(row, col, direction) {
+    row += directions[direction][0];
+    col += directions[direction][1];
+    return [row, col];
+}
+
+matrix(5)
 
 exports.solution = matrix;
