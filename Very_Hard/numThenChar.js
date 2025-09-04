a68aef262ea6e58776c66f382bd469865e7b5ef2
itaiglikman <itaig1998@gmail.com>
2025-09-04 14:09:14 +0300

    num then chars

--- Very_Hard/numThenChar.js
+++ Very_Hard/numThenChar.js
@@ -30,8 +30,46 @@ Notes
 Test cases will containg integer and float numbers and single letters.
 */
 
-function numThenChar( /*args*/ ) {
-  //your code
+function numThenChar(bigA) {
+    const arrLengths = [];
+    let totalLength = 0;
+    const nums = [];
+    const chars = [];
+    
+    for (let i = 0; i < bigA.length; i++) {
+        for (let j = 0; j < bigA[i].length; j++) {
+            isNaN(bigA[i][j]) ? chars.push(bigA[i][j]) : nums.push(bigA[i][j]);
+        }
+        totalLength += bigA[i].length;
+        arrLengths.push(bigA[i].length);
+    }
+
+    const allSorted = getAllSorted(nums, chars);
+    let position = 0;
+    const newSortedBigA = []
+    for (const currLength of arrLengths) {
+        const arr = []
+        for (let i = 0; i < currLength; i++) {
+            arr.push(allSorted[position]);
+            position++;
+        }
+        newSortedBigA.push(arr);
+    }
+
+    console.log(newSortedBigA);
+    return newSortedBigA;
 }
+
+function getAllSorted(nums, chars) {
+    return nums.sort((a, b) => a - b).concat(chars.sort())
+}
+
+// numThenChar([
+//     [1, 2, 4.4, "f", "a", "b"],
+//     [0], [0.5, "d", "X", 3, "s"],
+//     ["f", "e", 8],
+//     ["p", "Y", "Z"],
+//     [12, 18]
+// ])
 
 exports.solution = numThenChar;
